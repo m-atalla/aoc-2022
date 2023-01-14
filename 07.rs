5afb7f085937a7cf970a07941d42486f27671e10
m-atalla <mo.atalla96@gmail.com>
2023-01-14 16:48:03 +0200

    add 07 solution

--- 07.rs
+++ 07.rs
@@ -1,66 +1,169 @@
-use std::rc::{Rc, Weak};
-use std::collections::HashMap;
-
 #[derive(Debug, Clone)]
-struct Node {
-    name: String,
-    size: usize,
-    children: HashMap<String, Rc<Node>>,
-    parent: Option<Weak<Node>>
+enum Node {
+    Dir(String, Vec<Node>),
+    File(usize)
 }
+
 
 impl Node {
-    pub fn new(name: &str, size: usize) -> Node {
-        Node { 
-            name: name.to_string(), 
-            size, 
-            children: HashMap::default(),
-            parent: None
+    fn from_str(s: &str) -> Self {
+        let (lhs, rhs) = s.split_once(" ").unwrap();
+
+        return match lhs {
+            "dir" => Node::Dir(rhs.to_string(), Vec::new()),
+            _ => Node::File(lhs.parse().unwrap())
+        };
+    }
+}
+
+
+#[derive(Debug)]
+struct FileSystem {
+    pub current: Node, 
+    pub stack: Vec<Node>
+}
+
+impl FileSystem {
+    pub fn new(root: Node) -> Self {
+        Self {
+            current: root,
+            stack: Vec::new(),
         }
     }
 }
 
-#[inline]
-fn is_command(line: &str) -> bool {
-    line.starts_with("$")
-}
 
-fn process_cmd<'a>(line: &str, mut current_dir: Node, mut lines: impl Iterator<Item = &'a str>) {
-    println!("line .... {}", line);
-    if line == "ls" {
-        println!("got an ls");
+fn process_input(input: &str, fs: &mut FileSystem) {
+    for line in input.lines().skip(1) { 
 
-        let output = lines.by_ref().take_while(|&line| !is_command(&line));
+        let parts = line.split(" ").collect::<Vec<_>>();
 
-        for l in output {
-            println!("Output: {l}");
-        }
+        if line.starts_with("$"){
+            match parts.get(1) {
+                Some(&"cd") => {
 
-        for l in lines.take(5) {
-            println!("next: {l}"); 
-        }
+                    // updating stack
+                    let mut old = fs.current.clone();
 
-        std::process::exit(0);
-    } else {
-        // cd cmd
-        let (_, arg) = line.split_once(" ").unwrap();
+                    // Processing argument
+                    match parts.get(2).unwrap() {
+                        // move to parent.. e.g. unwind the stack
+                        &".." => {
+                            let parent = fs.stack.pop().unwrap();
+                            
+                            fs.current = parent;
 
-        if current_dir.children.contains_key(arg) {
-            // current_dir = *current_dir.children.get(arg).unwrap().clone();
+                            if let Node::Dir(_, children) = &mut fs.current {
+                                children.push(old);
+                            }
+
+                            continue;
+                        },
+                        &arg => {
+                            if let Node::Dir(_, children) = &mut fs.current {
+                                for (idx, child) in children.iter().enumerate() {
+                                    match child {
+                                        Node::File(_) => continue,
+                                        Node::Dir(name, _) => {
+                                            if name.as_str() == arg {
+
+                                                if let Node::Dir(_, children) = &mut old {
+                                                    fs.current = children.remove(idx);
+                                                }
+
+                                                fs.stack.push(old);
+
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                continue;
+                            }
+                        },
+                    };
+
+                    // update current node
+
+                },
+                Some(&"ls") => continue,
+                _ => panic!("Invalid command")
+            }
+        } else {
+            if let Node::Dir(_, children) = &mut fs.current {
+                children.push(
+                    Node::from_str(line)
+                );
+            }
         }
     }
-
 }
 
+fn calc_size(node: &Node, mut dir_sizes: &mut Vec<usize>) -> usize {
+    match node {
+        Node::Dir(_, children) => {
+
+            let dir_size  = children
+                .iter()
+                .map(|child| calc_size(child, &mut dir_sizes))
+                .sum();
+
+            dir_sizes.push(dir_size);
+
+            dir_size
+        },
+        &Node::File(size) => {
+            size
+        }
+    }
+}
+
+fn part_1(input: &str) -> (Vec<usize>, usize){
+    let mut fs = FileSystem::new(
+        Node::Dir("/".to_string(), Vec::new())
+    );
+
+    process_input(&input, &mut fs);
+
+    let root = fs.current;
+    let mut dir_sizes = Vec::new();
+
+
+    let root_size = calc_size(&root, &mut dir_sizes);
+
+
+    // to be used in part 2
+    let dir_sizes_result = dir_sizes.clone();
+
+    let part_1: usize = dir_sizes
+        .into_iter()
+        .filter(|&s| s <= 100000)
+        .sum();
+
+    println!("part 1: {}", part_1);
+
+    return (dir_sizes_result, root_size)
+}
+
+const DISK_SPACE: usize = 70000000;
+const UPDATE_SPACE: usize = 30000000;
 fn main() {
-    let input = std::fs::read_to_string("./inputs/07.in").unwrap();
+    let input = std::fs::read_to_string("./inputs/07.in")
+        .unwrap();
+    
+    let (mut dir_sizes, root_size) = part_1(&input);
 
-    let mut root = Node::new("/", 0);
+    let free_space = DISK_SPACE - root_size;
 
-    let mut current_dir = root.clone();
+    let required_space = UPDATE_SPACE - free_space;
 
-    let mut lines = input.lines().skip(1);
+    dir_sizes.sort();
 
-    let line = lines.next().unwrap();
-    process_cmd(&line[2..].trim(), current_dir, lines);
-} 
+    for size in dir_sizes {
+        if size >= required_space {
+            println!("part 2: {}",size);
+            break;
+        }
+    }
+}
